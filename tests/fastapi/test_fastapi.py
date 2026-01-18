120b9f154bf06aaeb3a5c9fc6ca9a423009f4e49
Quinn Blenkinsop <quinn@arcjet.com>
2026-01-18 06:54:25 -0500

    chore(tests): add fastapi & flask specfic tests (#17)
    Co-authored-by: David Mytton <david@arcjet.com>

---
+++ tests/fastapi/test_fastapi.py
@@ -0,0 +1,90 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
+from fastapi.testclient import TestClient
+import pytest
+
+from arcjet import arcjet as arcjet_fastapi
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+
+    arcjet = arcjet_fastapi(
+        # TODO: figure out why `invalid.local` causes tests to take minutes.
+        base_url="https://invalid.test",  # Intentional invalid URL for testing
+        key="ajkey_dummy",
+        rules=[],
+    )
+
+    @app.get("/protected")
+    async def protected_route(request: Request):
+        decision = await arcjet.protect(request)
+
+        if decision.is_error():
+            return PlainTextResponse(
+                f'Internal Server Error: "${decision.reason}', status_code=500
+            )
+        if decision.is_allowed():
+            return PlainTextResponse("Ok", status_code=200)
+        if decision.is_denied():
+            return PlainTextResponse("Forbidden", status_code=403)
+
+        pytest.fail("Unexpected decision state.")
+
+    return app
+
+
+def test_basic_get():
+    app = create_app()
+    client = TestClient(app)
+
+    response = client.get("/protected")
+
+    assert response.status_code == 200, f"Unexpected status: {response.text}"
+
+
+def test_basic_get_with_search_parameter():
+    app = create_app()
+    client = TestClient(app)
+
+    response = client.get("/protected?alpha=bravo")
+
+    assert response.status_code == 200, f"Unexpected status: {response.text}"
+
+
+def test_basic_get_with_hash():
+    app = create_app()
+    client = TestClient(app)
+
+    response = client.get("/protected#alpha-bravo")
+
+    assert response.status_code == 200, f"Unexpected status: {response.text}"
+
+
+def test_basic_get_with_headers():
+    app = create_app()
+    client = TestClient(app)
+
+    headers = {
+        "X-Custom-Header": "CustomValue",
+        "User-Agent": "ArcjetTestClient/1.0",
+    }
+
+    response = client.get("/protected", headers=headers)
+
+    assert response.status_code == 200, f"Unexpected status: {response.text}"
+
+
+def test_basic_get_with_cookies():
+    app = create_app()
+
+    cookies = {
+        "session_id": "abc123",
+        "user_pref": "dark_mode",
+    }
+
+    client = TestClient(app, cookies=cookies)
+
+    response = client.get("/protected")
+
+    assert response.status_code == 200, f"Unexpected status: {response.text}"
---
+++ tests/flask/test_flask.py
@@ -0,0 +1,83 @@
+from flask import Flask, request
+import pytest
+
+from arcjet import arcjet_sync as arcjet_flask
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+
+    arcjet = arcjet_flask(
+        # TODO: figure out why `invalid.local` causes tests to take minutes.
+        base_url="https://invalid.test",  # Intentional invalid URL for testing
+        key="ajkey_dummy",
+        rules=[],
+    )
+
+    @app.route("/protected", methods=["GET"])
+    def protected_route():
+        decision = arcjet.protect(request)
+
+        if decision.is_error():
+            return f'Internal Server Error: "${decision.reason}', 500
+        if decision.is_allowed():
+            return "Ok", 200
+        if decision.is_denied():
+            return "Forbidden", 403
+
+        pytest.fail("Unexpected decision state.")
+
+    return app
+
+
+def test_basic_get():
+    app = create_app()
+    client = app.test_client()
+
+    response = client.get("/protected")
+
+    assert response.status_code == 200, f"Unexpected status: {response.text}"
+
+
+def test_basic_get_with_search_parameter():
+    app = create_app()
+    client = app.test_client()
+
+    response = client.get("/protected?alpha=bravo")
+
+    assert response.status_code == 200, f"Unexpected status: {response.text}"
+
+
+def test_basic_get_with_hash():
+    app = create_app()
+    client = app.test_client()
+
+    response = client.get("/protected#alpha-bravo")
+
+    assert response.status_code == 200, f"Unexpected status: {response.text}"
+
+
+def test_basic_get_with_headers():
+    app = create_app()
+    client = app.test_client()
+
+    headers = {
+        "X-Custom-Header": "CustomValue",
+        "User-Agent": "ArcjetTestClient/1.0",
+    }
+
+    response = client.get("/protected", headers=headers)
+
+    assert response.status_code == 200, f"Unexpected status: {response.text}"
+
+
+def test_basic_get_with_cookies():
+    app = create_app()
+
+    client = app.test_client()
+    client.set_cookie("alpha", "bravo")
+    client.set_cookie("charlie", "delta")
+
+    response = client.get("/protected")
+
+    assert response.status_code == 200, f"Unexpected status: {response.text}"
