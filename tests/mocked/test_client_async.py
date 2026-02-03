d8e7080ff383aa6483b2c7cb9f996b385f6710bc
David Mytton <david@arcjet.com>
2026-02-03 17:15:19 -0500

    use same proxies config as other tests

--- tests/mocked/test_client_async.py
+++ tests/mocked/test_client_async.py
@@ -118,7 +118,7 @@ def test_disable_automatic_ip_detection_with_proxies():
         key="ajkey_x",
         rules=rules,
         disable_automatic_ip_detection=True,
-        proxies={"http": "http://proxy.example.com:8080"},
+        proxies=["3.3.3.3"],
     )
     import asyncio
 
