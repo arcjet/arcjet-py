0fa04a9cc74fe64b97c95dfd298ab73770e5539c
David Mytton <david@arcjet.com>
2026-02-14 12:50:53 -0500

    fix: fix minor typos (#47)
    Correct minor typos

--- examples/fastapi/main.py
+++ examples/fastapi/main.py
@@ -30,8 +30,8 @@ aj = arcjet(
                 BotCategory.SEARCH_ENGINE,  # Google, Bing, etc
                 # Uncomment to allow these other common bot categories
                 # See the full list at https://arcjet.com/bot-list
-                # BotCategory.MONITOR", # Uptime monitoring services
-                # BotCategory.PREVIEW", # Link previews e.g. Slack, Discord
+                # BotCategory.MONITOR, # Uptime monitoring services
+                # BotCategory.PREVIEW, # Link previews e.g. Slack, Discord
             ],
         ),
         # Create a token bucket rate limit. Other algorithms are supported
